371c70bd3a907371a2021b5a6bd6c369cc4081c5
Boxie <forkome@gmail.com>
2022-11-18 22:59:59 +0800

    增加測試單

--- src/PMS/manage.py
+++ src/PMS/manage.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                           'PMS.settings.test-box-local')
 
-    from django.conf import settings
-    if DEBUG:
-        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-            import debugpy
-            debugpy.listen(("0.0.0.0", 3000))
-            print('Attached!')
+    # from django.conf import settings
+    # if DEBUG:
+    #     if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+    #         import debugpy
+    #         debugpy.listen(("0.0.0.0", 3000))
+    #         print('Attached!')
 
     try:
         from django.core.management import execute_from_command_line
