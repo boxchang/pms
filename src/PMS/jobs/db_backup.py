9e97f5248c8c377f51a4b81d13026f6f3cdba87c
Box Chang <forkome@gmail.com>
2023-01-09 15:06:43 +0800

    Update

--- src/PMS/jobs/db_backup.py
+++ src/PMS/jobs/db_backup.py
@@ -6,29 +6,18 @@ import datetime
 now = datetime.datetime.now()
 file_name = datetime.datetime.strftime(now,'%Y%m%d %H%M%S')
 delete_keyword = datetime.datetime.strftime((now - datetime.timedelta(days = 3)), '%Y%m%d')
-print(delete_keyword)
-print(file_name)
-
-print(os.path.abspath(os.path.dirname(__file__)))
-print(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 src = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'db.sqlite3')
 
-dst_path = 'C:\\Users\\hsiangchih.chang\\Desktop\\Temp\\'
+dst_path = 'S:\\管理部\\資訊管理課\\110_資產管理系統資料庫備份\\'
 dst = dst_path + file_name + ".sqlite3"
 
 for dirpath, dirName, filenames in os.walk(dst_path):
     for file in filenames:
-        if "20230106" in file:
+        if delete_keyword in file:
             print(file)
             os.remove(dst_path+file)
 
+result = os.path.isfile(src)
+if result:
+    shutil.copyfile(src, dst)
 
-# print(src)
-# result = os.path.isfile(src)
-# if result:
-#     shutil.copyfile(src, dst)
-# print(result)
-
-# src = 'C:\\Users\\hsiangchih.chang\\Desktop\\Temp\\Invoice.pdf'
-# dst = 'C:\\Users\\hsiangchih.chang\\Desktop\\Temp\\Invoice2.pdf'
-# shutil.copyfile(src, dst)
