5ccf9226d63e277fb3175d4a9714b041e1351932
Box <forkome@gmail.com>
2023-05-17 10:11:58 +0800

    Update

--- src/PMS/borrow/test.py
+++ src/PMS/borrow/test.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 conn = pyodbc.connect("DRIVER={{SQL Server}};SERVER={server}; database={database}; \
-                   trusted_connection=yes;UID={uid};PWD={pwd}".format(server="10.96.101.4", database="EFGP_Test", uid="sa", pwd="134"))
+                   trusted_connection=no;UID={uid};PWD={pwd}".format(server="10.96.101.4", database="EFGP_Test", uid="sa", pwd="134"))
 sql = """select id,organizationUnitName 
              from OrganizationUnit where validType = 1 and managerOID is not null order by id"""
 
