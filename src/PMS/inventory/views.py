146017b23b40deb543d9d7ea2fa269492e874c3b
Box <forkome@gmail.com>
2023-11-16 08:52:54 +0800

    Update

--- src/PMS/inventory/views.py
+++ src/PMS/inventory/views.py
@@ -337,7 +337,7 @@ def apply(request):
         return redirect(apply.get_absolute_url())
 
     form = OfficeInvForm(initial={"unit": request.user.unit, "requester": request.user})
-    form.fields["requester"].queryset = CustomUser.objects.filter(unit=request.user.unit).all()
+    form.fields["requester"].queryset = CustomUser.objects.filter(unit=request.user.unit, is_active=True).all()
     search_form = SearchForm()
     attach_form = AttachmentForm()
     return render(request, 'inventory/application.html', locals())
