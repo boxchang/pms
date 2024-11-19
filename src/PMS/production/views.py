22eb148f0e19007e22c17e11d87f4e77430e6753
Boxie <forkome@gmail.com>
2024-11-19 16:15:15 +0700

    Update

--- src/PMS/production/views.py
+++ src/PMS/production/views.py
@@ -539,11 +539,13 @@ def get_step_info(request):
                 if wodata_pre_step.ctr_code == "YY05":
                     value['pre_step_done'] = "Y"
                 else:
-                    pre_step_good_rows = Record.objects.filter(wo_no=step.wo_main.wo_no, good_qty__gt=0).order_by('-update_at')
-                    if pre_step_good_rows:
-                        pre_step_good_qty = pre_step_good_rows[0].good_qty
-                    else:
-                        pre_step_good_qty = 0
+                    record_pre_step = Record.objects.filter(wo_no=step.wo_main.wo_no, good_qty__gt=0).order_by('-update_at')[0]
+
+                    pre_step_good_rows = Record.objects.filter(wo_no=step.wo_main.wo_no,
+                                                               step_no=record_pre_step.step_no).aggregate(
+                        Sum('good_qty'))
+                    pre_step_good_qty = pre_step_good_rows['good_qty__sum'] if pre_step_good_rows[
+                        'good_qty__sum'] else 0
 
                     if (pre_step_good_qty + wo_ng_qty) >= step.wo_qty:
                         value['pre_step_done'] = "Y"
